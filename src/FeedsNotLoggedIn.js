dde6269ee6204a071886619a0d92af1353146bcd
Eilaf-Alg <eilaf.algebail@gmail.com>
2019-08-03 12:51:31 +0400

    added an icon to FeedsNotLoggedIn.js

--- src/FeedsNotLoggedIn.js
+++ src/FeedsNotLoggedIn.js
@@ -15,7 +15,6 @@ const Feeds = () => {
             <NavBar />
             <div className="container">
                 <br/> <br/>
-
                 <h1> <i classclassName="fas fa-sign-in-alt"></i> </h1>
                 <h1> Please Log in to see your feeds :) </h1>
             </div>
